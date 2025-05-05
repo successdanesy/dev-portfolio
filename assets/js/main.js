81caad717f573df9a20128a7d4a096f30af66d6d
Success <stealph14@gmail.com>
2025-05-05 12:28:46 +0100

    changes

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*Menu Show*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    // Validate that variables exist
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
